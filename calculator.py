198e1920af1ea96b7c038576ad602520f623098b
Axumawityemaneh <yemanehaxumawit@gmail.com>
2024-04-22 11:31:06 +0300

    added new files

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    result=x+y
+    return result
+def subtract(x,y):
+    result=x-y
+    return result
+def divide(x,y):
+    result=x/y
+    return result
+def multiply(x,y):
+    result=x*y
+    return result
+def remainder(x,y):
+    result=x%y
+    return result
+def powerof(h,i):
+    result=h**i
+    return result
---
+++ hello.py
@@ -0,0 +1,11 @@
+def hello(name): 
+    print(f"Hello {name}")
+
+
+def year_of_birth(name,age):
+    year=2024 - age
+    print(f"Hello {name}, you were born in{year}")
+
+def my_country(country="Uganda"):
+    print(f"Hello Akirachix from{country}")
+    
